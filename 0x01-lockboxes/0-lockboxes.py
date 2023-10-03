3a4354fd578f36fd3e66974f54b4a0bbac6c6ef1
Frank <wanyoike39@gmail.com>
2023-10-03 11:30:24 +0300

    lockboxes algorithm - removing white spaces

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,11 +6,11 @@ def canUnlockAll(boxes):
     """function to check if all boxes can be opened"""
     if boxes is None or len(boxes) == 0:
         return False
-    
+
     keys = [0]
     for key in keys:
         for new_key in boxes[key]:
             if new_key not in keys and new_key < len(boxes):
                 keys.append(new_key)
-    
+
     return len(keys) == len(boxes)
