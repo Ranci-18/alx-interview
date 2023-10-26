9f5b16b5df3481cbd91d5170eebcdc4b64f3dba3
Frank <wanyoike39@gmail.com>
2023-10-27 00:45:03 +0300

    utf-8 validation script

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,8 @@ def validUTF8(data):
     """Determines if a given data set represents a valid
     UTF-8 encoding"""
     for unit_data in data:
-        
+        if not (0 <= unit_data < 256):
+            return False
         byte_data = bytes([unit_data])
         try:
             codecs.decode(byte_data, 'utf-8', errors='strict')
