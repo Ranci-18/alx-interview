6d19da668df82f1d443ed380e87262af4d82d882
Frank <wanyoike39@gmail.com>
2023-11-09 19:36:49 +0300

    starwars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,17 +8,29 @@ request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, function (error
   if (response.statusCode === 200) {
     const parsedData = JSON.parse(body);
     const characterAPIList = parsedData.characters;
-    for (const characterAPI of characterAPIList) {
+    /* for (const characterAPI of characterAPIList) {
       request(characterAPI, function (err, res, body) {
         if (res.statusCode === 200) {
           const characterParsedData = JSON.parse(body);
-          console.log(characterParsedData.name);
-        } else {
-          console.log(err);
-        }
-      });
-    }
+          console.log(characterParsedData.name); */
+    printNames(characterAPIList, 0);
   } else {
     console.log(error);
   }
 });
+
+const printNames = (characterAPIList, index) => {
+  if (index === characterAPIList.length) {
+    return;
+  }
+
+  request(characterAPIList[index], function (err, res, body) {
+    if (res.statusCode === 200) {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      printNames(characterAPIList, index + 1);
+    } else {
+      console.log(err);
+    }
+  });
+};
