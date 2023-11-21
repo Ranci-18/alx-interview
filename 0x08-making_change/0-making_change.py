cd05c4cb5c574b0401acc9ec2014c86fd02af28e
Frank <wanyoike39@gmail.com>
2023-11-21 10:03:37 +0300

    make change script

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,7 @@ def makeChange(coins, total):
     change returned for the total"""
     if total <= 0:
         return 0
-    
+
     number_of_coins = 0
     coins.sort(reverse=True)
     initial_total = total
@@ -17,15 +17,13 @@ def makeChange(coins, total):
             if denomination <= total:
                 total -= denomination * num_timez_to_minus
                 number_of_coins += num_timez_to_minus
-                
+
         if total == initial_total:
             break
 
         initial_total = total
-    
+
     if total == 0:
         return number_of_coins
     else:
         return -1
-
-   
